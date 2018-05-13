73053a2c5622bc56b23dceedf8a27a2abd4f5370
Gusto JS <gustojs@protonmail.com>
2018-05-13 05:41:22 +0200

    fixed feathersClient module path

--- store/index.js
+++ store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import feathersVuex from 'feathers-vuex';
-import feathersClient from '~/plugins/feathers';
+import feathersClient from '../feathers-client';
 const { auth, FeathersVuex } = feathersVuex(feathersClient, { idField: '_id' });
 Vue.use(Vuex);
 Vue.use(FeathersVuex);
